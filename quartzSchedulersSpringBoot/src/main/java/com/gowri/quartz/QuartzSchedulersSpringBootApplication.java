c9c501c2fa36d9fa63f5272a97494ebaffa18e27
Naveen <naveenkrishnawodeyar@gmail.com>
2024-10-09 18:23:12 +0530

    quartzSchedulers initial commit

---
+++ quartzSchedulersSpringBoot/src/main/java/com/gowri/quartz/QuartzSchedulersSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.gowri.quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzSchedulersSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzSchedulersSpringBootApplication.class, args);
+		System.out.println("\n***********************\n");
+	}
+
+}
---
+++ quartzSchedulersSpringBoot/src/test/java/com/gowri/quartz/QuartzSchedulersSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gowri.quartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzSchedulersSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
