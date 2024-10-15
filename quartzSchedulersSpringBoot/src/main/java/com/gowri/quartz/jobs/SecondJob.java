d7ea0c7b6889662ec5495eff3396303f8180eeb0
Naveen <naveenkrishnawodeyar@gmail.com>
2024-10-15 21:43:05 +0530

    time stats added for the job

--- quartzSchedulersSpringBoot/src/main/java/com/gowri/quartz/jobs/SecondJob.java
+++ quartzSchedulersSpringBoot/src/main/java/com/gowri/quartz/jobs/SecondJob.java
@@ -1,5 +1,8 @@
 package com.gowri.quartz.jobs;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -16,9 +19,30 @@ public class SecondJob implements Job {
 	private static final Logger log = LoggerFactory.getLogger(SecondJob.class);
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
-		log.info("\n**********************\n");
-		System.out.println("SECOND JOB");
-		log.info("\n**********************\n");
-	}
+        long startTime = System.currentTimeMillis();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("############################");
+        log.info("\n********** Started ************\n");
+        log.info("Start Time: {}", sdf.format(new Date(startTime)));
+
+        try {
+        	log.info("executing job");
+            System.out.println("SECOND JOB");
+
+        } catch (Exception e) {
+            log.error("Error executing SecondJob", e);
+            throw new JobExecutionException(e);
+        } finally {
+            long endTime = System.currentTimeMillis();
+            long duration = endTime - startTime;
+
+            log.info("End Time: {}", sdf.format(new Date(endTime)));
+            log.info("Total Duration: {} ms", duration);
+            System.out.println("End Time: " + sdf.format(new Date(endTime)));
+            System.out.println("Total Duration: " + duration + " ms");
+            log.info("\n********** Finished ************\n");
+            System.out.println("############################");
+        }
+    }
 
 }
