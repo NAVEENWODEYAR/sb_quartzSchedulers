6b17705c4139b31d8593d1c6db8a3a6efddd078b
Naveen <naveenkrishnawodeyar@gmail.com>
2024-12-19 21:18:19 +0530

    quartz script added

--- quartzSchedulersSpringBoot/src/main/java/com/gowri/quartz/QuartzSchedulersSpringBootApplication.java
+++ quartzSchedulersSpringBoot/src/main/java/com/gowri/quartz/QuartzSchedulersSpringBootApplication.java
@@ -18,39 +18,29 @@ public class QuartzSchedulersSpringBootApplication {
 
     private static final Logger log = LoggerFactory.getLogger(QuartzSchedulersSpringBootApplication.class);
 
-    // Date format for logging timestamps
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     
-    // Variable to hold the application start time
     private static Date startTime;
 
     public static void main(String[] args) {
-        // Run the Spring application and get the application context
         ApplicationContext context = SpringApplication.run(QuartzSchedulersSpringBootApplication.class, args);
         
-        // Access the environment to retrieve properties
         Environment env = context.getEnvironment();
 
-        // Retrieve the active profiles, defaulting to "default" if none are set
         String activeProfile = getActiveProfile(env);
         
-        // Get the name of the properties file used (default is "application")
         String propertySourceName = env.getProperty("spring.config.name", "application");
         
-        // Record the application start time
         startTime = new Date();
 
-        // Log application startup details
         logStartupInfo(activeProfile, propertySourceName);
     }
 
-    // Helper method to retrieve the active profile
     private static String getActiveProfile(Environment env) {
         String[] activeProfiles = env.getActiveProfiles();
         return activeProfiles.length > 0 ? activeProfiles[0] : "default";
     }
 
-    // Logs application startup information
     private static void logStartupInfo(String activeProfile, String propertySourceName) {
         System.out.println("\n***********************");
         System.out.println("Active Profile: " + activeProfile);
